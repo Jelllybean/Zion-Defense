da48788b3e328743897192ab74c3cd76e818a55f
Unknown <jellesimonvds@gmail.com>
2019-10-17 23:01:30 +0200

    button now works!

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Transform object1;
+    public Transform object2;
+
+    void Update()
+    {
+        float distance = Vector3.Distance(object1.position, object2.position);
+        print(distance);
+    }
+}
